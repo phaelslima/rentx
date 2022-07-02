fbbeae67844936345435bad4ee11978aa1feb49e
Raphael Lima <phaelslima@gmail.com>
2022-07-02 10:44:28 -0300

    feat: add cartDetails screen

---
+++ src/screens/CarDetails/index.tsx
@@ -0,0 +1,76 @@
+import React from 'react'
+
+import { BackButton } from '../../components/BackButton'
+import { ImageSlider } from '../../components/ImageSlider'
+import { Accessory } from '../../components/Accessory'
+
+import SpeedSvg from '../../assets/speed.svg'
+import AccelerationSvg from '../../assets/acceleration.svg'
+import ForceSvg from '../../assets/force.svg'
+import GasolineSvg from '../../assets/gasoline.svg'
+import ExchangeSvg from '../../assets/exchange.svg'
+import PeopleSvg from '../../assets/people.svg'
+
+import {
+  Container,
+  Header,
+  CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About,
+  Accessories,
+  Footer
+} from './styles'
+import { Button } from '../../components/Button'
+
+export function CarDetails(){
+  return (
+    <Container>
+      <Header>
+        <BackButton onPress={() => {}} />
+      </Header>
+
+      <CarImages>
+        <ImageSlider imagesUrl={['https://www.pngmart.com/files/1/Audi-RS5-Red-PNG.png']} />
+      </CarImages>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Lamborghini</Brand>
+            <Name>Huracan</Name>
+          </Description>
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 580</Price>
+          </Rent>
+        </Details>
+
+        <Accessories>
+          <Accessory name="380Km/h" icon={SpeedSvg} />
+          <Accessory name="3.2s" icon={AccelerationSvg} />
+          <Accessory name="800 HP" icon={ForceSvg} />
+          <Accessory name="Gasolina" icon={GasolineSvg} />
+          <Accessory name="Auto" icon={ExchangeSvg} />
+          <Accessory name="2 pessoas" icon={PeopleSvg} />
+        </Accessories>
+
+        <About>
+          Este é automóvel desportivo. Surgiu do lendário touro de lide indultado na praça Real Maestranza de Sevilla. É um belíssimo carro para quem gosta de acelerar.
+        </About>
+
+      </Content>
+
+      <Footer>
+        <Button title="Confirmar" />
+      </Footer>
+
+    </Container>
+  )
+}
