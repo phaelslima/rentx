8dfa13035417758f4acc887e24c79f6a67e71a21
Raphael Lima <phaelslima@gmail.com>
2022-07-03 22:57:37 -0300

    feat: add details on home screen

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,14 +4,28 @@ import { RFValue } from 'react-native-responsive-fontsize'
 
 import Logo from '../../assets/logo.svg'
 
+import { Car } from '../../components/Car'
+
 import {
   Container,
   Header,
   HeaderContent,
-  TotalCars
+  TotalCars,
+  CarList,
+  Separator
 } from './styles'
 
-export function Home(){
+export function Home() {
+  const cartData = {
+    brand: 'audi',
+    name: 'RS 5 Coupé',
+    rent: {
+      period: 'ao dia',
+      price: 120
+    },
+    thumbnail: 'https://www.pngmart.com/files/1/Audi-RS5-Red-PNG.png'
+  }
+
   return (
     <Container>
       <StatusBar
@@ -30,6 +44,13 @@ export function Home(){
           </TotalCars>
         </HeaderContent>
       </Header>
+
+      <CarList
+        data={[1,2,3,4,5,6,7,8,9,10,11,12,13]}
+        keyExtractor={item => String(item)}
+        renderItem={({ item }) => <Car data={cartData} />}
+        ItemSeparatorComponent={() => <Separator />}
+      />
     </Container>
   )
 }
